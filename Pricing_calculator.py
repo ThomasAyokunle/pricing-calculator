727275f2f3624731aff663c1b3b5b916a26b9d17
Thomas Ayokunle <133719533+ThomasAyokunle@users.noreply.github.com>
2025-11-06 08:04:46 +0100

    Update Pricing_calculator.py

--- Pricing_calculator.py
+++ Pricing_calculator.py
@@ -8,7 +8,7 @@ import math
 import numpy as np
 
 # --- PAGE CONFIG ---
-st.set_page_config(page_title="Laboratory Pricing Calculator", layout="wide")
+st.set_page_config(page_title="D-Rock Laboratory Pricing Calculator", layout="wide")
 
 # --- HEADER ---
 st.title("Laboratory Pricing Calculator")
@@ -32,7 +32,7 @@ def load_sheet(sheet_name):
 # --- SIDEBAR CONTROLS ---
 st.sidebar.header("Simulation Controls")
 
-lab = st.sidebar.selectbox("Select Lab Location", ["OPIC_LAB", "CHEVRON_LAB"])
+lab = st.sidebar.selectbox("Select Lab Location", ["OPIC LAB", "CHEVRON LAB"])
 df = load_sheet(lab)
 
 selected_test = st.sidebar.selectbox("Select Test", df["TEST NAME"].unique())
@@ -192,6 +192,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 st.caption("ExCare Services Laboratory Pricing Calculator © 2025")
+
 
 
 
